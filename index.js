7bbd2f3fa63797591b9435a0daac44f3bb2f832d
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-04-25 12:17:54 -0700

    Made the port dynamic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require("dotenv").config(); //reads .env and merges it into process.env
+
 const server = require("./server.js");
 
-// listening
-server.listen(5000, () => {
-  console.log("\n*** Server is running on http://localhost:5000 ***\n");
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`\n*** Server is running on http://localhost:${port} ***\n`);
 });
+
+// Environment variables are system wide.
+// A hosting service might assign port dynamically.
